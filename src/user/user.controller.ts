cc94f4817afbe3a5671da631016dabf2901c199d
Ronny Pereira <ronny.pereira@ipdec.org>
2023-02-24 15:49:25 -0400

    primeira integração com o banco de dados

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Post } from '@nestjs/common';
-import { Body } from '@nestjs/common/decorators';
+import { Body, Get } from '@nestjs/common/decorators';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userService.getAllUser();
   }
 }
