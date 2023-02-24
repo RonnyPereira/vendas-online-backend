50721dc09071af07b38614b132d7c0475743dc30
Ronny Pereira <ronny.pereira@ipdec.org>
2023-02-24 16:37:32 -0400

    projeto com typeOrm e migration funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { createTableUser1677268460545 } from './migration/1677268460545-create_table_user';
+import { createTableState1677270442985 } from './migration/1677270442985-create_table_state';
+import { createTableCity1677270481303 } from './migration/1677270481303-create_table_city';
+import { createTableAddress1677270515824 } from './migration/1677270515824-create_table_address';
 import { UserModule } from './user/user.module';
+
+const migrations = [
+  createTableUser1677268460545,
+  createTableState1677270442985,
+  createTableCity1677270481303,
+  createTableAddress1677270515824,
+];
 
 @Module({
   imports: [
@@ -15,9 +26,10 @@ import { UserModule } from './user/user.module';
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
-      synchronize: true,
-      autoLoadEntities: true,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [...migrations],
+      migrationsRun: true,
+      autoLoadEntities: true,
     }),
     UserModule,
   ],
